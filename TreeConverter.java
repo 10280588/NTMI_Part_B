8740917718f118ffa78816dc2e7210891b14389a
Menno <mennovleeuwen1990@gmail.com>
2015-03-18 14:07:32 +0100

    Backup for possible major fuck up

--- TreeConverter.java
+++ TreeConverter.java
@@ -20,11 +20,11 @@ public class TreeConverter{
                 System.out.println("Tagged sentence: " + convertedString + "\n");
             }
         }
-        
+
         System.out.println("Done converting the file");
         return convertedString;
     }
-    
+
     public static ArrayList<ArrayList<String>> addTagsToBranch(ArrayList<ArrayList<String>> branch) {
     	ArrayList<ArrayList<String>> newBranch = new ArrayList<ArrayList<String>>();
     	String currentEditedPart = "(@";
@@ -37,10 +37,10 @@ public class TreeConverter{
     		newBranch.add(branch.get(0));
     		newBranch.add(branch.get(1));
     		tagPart = branch.get(0).get(0);
-    		tagPart = tagPart.replace("(", ""); 
+    		tagPart = tagPart.replace("(", "");
     		currentEditedPart += tagPart + "->";
     		int bracketsToAdd = branch.size() - 2;
-    		for(int i = 2; i < branch.size(); i++) {    			
+    		for(int i = 2; i < branch.size(); i++) {
     			tagPart = "_" + branch.get(i-1).get(0);
     			tagPart = tagPart.replace("(", "");
     			currentEditedPart += tagPart;
@@ -61,7 +61,7 @@ public class TreeConverter{
     	}
     	return newBranch;
     }
-    
+
     public static String getTags(String[] sentence) {
     	ArrayList<ArrayList<String>> currentBranch = null;
     	ArrayList<ArrayList<String>> branches = getBranches(sentence);
@@ -74,12 +74,12 @@ public class TreeConverter{
 					if(currentBranch.size() > 1) {
 						System.out.println(currentBranch);
 						taggedBranchList.add(currentBranch);
-					}  
+					}
 					String[] subBranchSentence = subBranchToStringArray(subBranch);
-					ArrayList<ArrayList<String>> subbranches = getBranches(subBranchSentence); 
+					ArrayList<ArrayList<String>> subbranches = getBranches(subBranchSentence);
 					branches = subbranches;
 					lastBranch = branches;
-				}	 
+				}
 	    	} else {
 	    		System.out.println("breaking");
 	    		break;
@@ -88,15 +88,15 @@ public class TreeConverter{
 		if(lastBranch.size() > 1) {
 			System.out.println(lastBranch);
 			taggedBranchList.add(lastBranch);
-		} 
-		
+		}
+
 		StringBuilder sb = new StringBuilder();
         for(String str : branchToStringArray(currentBranch)) sb.append(str + " ");
         return sb.toString();
     }
-    
 
-    
+
+
     public static boolean hasMoreTags(ArrayList<ArrayList<String>> branch) {
     	boolean tags = false;
     	if(branch.size() > 2) {
@@ -110,17 +110,17 @@ public class TreeConverter{
     	}
     	return tags;
     }
-    
+
     public static String[] branchToStringArray(ArrayList<ArrayList<String>> branch) {
     	String branchPart = "";
         for(int j = 0; j < branch.size(); j++) {
 			 for(int k = 0; k < branch.get(j).size(); k++) {
-				 branchPart += branch.get(j).get(k) + " ";                     		 
+				 branchPart += branch.get(j).get(k) + " ";
 			 }
         }
         return branchPart.split(" ");
     }
-    
+
     public static String[] subBranchToStringArray(ArrayList<String> subBranch) {
     	String branchPart = "";
         for(int j = 0; j < subBranch.size(); j++) {
@@ -135,13 +135,13 @@ public class TreeConverter{
         subBranch.add(sentence[0]);
         int numbOfClose;
         ArrayList<ArrayList<String>> branch = new ArrayList<ArrayList<String>>();
-        
+
         branch.add(subBranch);
         subBranch = new ArrayList<String>();
-        for(int i = 1; i < sentence.length; i++) {  	
+        for(int i = 1; i < sentence.length; i++) {
         	if (sentence[i].contains("(")) {
                 openCounter++;
-                subBranch.add(sentence[i]);        
+                subBranch.add(sentence[i]);
             } else if (sentence[i].contains(")")) {
             	numbOfClose = sentence[i].length() - sentence[i].replace(")", "").length();
             	openCounter-=numbOfClose;
@@ -156,6 +156,6 @@ public class TreeConverter{
                 }
             }
         }
-        return branch;
+        return branch; 
     }
 }
